417bbba5f2dff8e3030673088e960612f296bf13
gonwms <gon.williams@gmail.com>
2026-01-13 20:35:32 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 import { defineConfig } from 'astro/config';
 
 
-// Para que funcione en una carpeta dentro del dominio agregar el la carpeta
+/*
+* OPCION 1 Para que funcione en una carpeta dentro del dominio agregar el la carpeta
+* Descomentar para exportar para mediabros.cl/lightman
+*/
+
+// export default defineConfig({
+//   base: '/lightman/',
+//   build: {
+//     assetsPrefix: '/lightman/'
+//   }
+// });
+
+/*
+* OPCION 2 Para que funcione en Vercel
+* Descomentar para exportar para Vercel
+*/
 export default defineConfig({
-  // base: '/lightman/',
-  // build: {
-  //   assetsPrefix: '/lightman/'
-  // }
   base: './',
   build: {
     assetsPrefix: './'
