99355f2b3e9858502b59a85abb215394a07eeda7
gonwms <gon.williams@gmail.com>
2026-01-15 17:56:27 -0300

    opengraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
-
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 /*
-* OPCION 1 Para que funcione en una carpeta dentro del dominio agregar el la carpeta
-* Descomentar para exportar para mediabros.cl/lightman
-*/
+ * OPCION 1 Para que funcione en una carpeta dentro del dominio agregar el la carpeta
+ * Descomentar para exportar para mediabros.cl/lightman
+ */
 
-const isVercel = process.env.VERCEL === '1' || process.env.NODE_ENV === 'development';
-const BASE_PATH = isVercel ? '/' : '/lightman/';
+const isVercel =
+  process.env.VERCEL === "1" || process.env.NODE_ENV === "development";
+const BASE_PATH = isVercel ? "/" : "/lightman/";
 
 export default defineConfig({
+  // site: "https://mediabros.cl",
   base: BASE_PATH,
-  integrations: [icon()]
+  integrations: [icon()],
 });
